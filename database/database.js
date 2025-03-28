b7ce17435d3cee7ade2a3dc31aa1a8d783c5079e
sebszuba <seba200202@gmail.com>
2025-03-28 03:02:00 -0500

    Revert changes (no fix)

--- database/database.js
+++ database/database.js
@@ -16,8 +16,7 @@ MYSQL_DATABASE='Project390DB'
 */
 
 
-//const pool = mysql.createPool({
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
